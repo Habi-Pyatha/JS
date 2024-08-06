b77baa55c2d9924003aa82eb5ea47c54cc676419
Habi-Pyatha <144538294+Habi-Pyatha@users.noreply.github.com>
2024-08-06 10:46:21 +0000

    conversion of data types

---
+++ basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score="ramesh"
+
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+// "33"=>33
+// "33ab"=>NaN
+// ture=>1 ;false=>0
+//
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true ; 0=>false
+
+let number=33
+
+let stringNumber=String(number)
+console.log(typeof stringNumber);
+
