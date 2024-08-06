b202f3f1bdee32986205def6aad04b6f561c966b
Habi-Pyatha <144538294+Habi-Pyatha@users.noreply.github.com>
2024-08-06 07:40:25 +0000

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) we are using nodejs , not browser
+
+console.log(3+4) // code readabilty should be high
+console.log("Grammer")
+
+
+let name="Hitesh"
+let age=18
+let isLoggedIn=true
+
+//number => 2^53
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId =144553
+let accountEmail="ram@gmail.com"
+var accountPassword= "12314"
+accountCity = "Jaipur"
+let accountState;
+/* never use var keyword it causes a lot problems
+causes issuus in block scope and funcitonal code */
+// accountId=2
+accountEmail="hello@yahoo.com"
+accountPassword="121212"
+accountCity="Bangaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
